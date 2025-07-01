abe9eace9866d2dc60ef81d1ada823fcb5c3260e
Aruli Azmi <36721202+aruliazmi@users.noreply.github.com>
2025-07-01 16:04:01 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  dialect: 'mysql',
+  logging: false
+});
+
+const PlayerUCP = sequelize.define('playerucp', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  code: {
+    type: DataTypes.STRING(5),
+    allowNull: false
+  },
+  discord_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  verified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  timestamps: true,
+  createdAt: 'reg_date',
+  updatedAt: false,
+  tableName: 'playerucp'
+});
+
+sequelize.sync();
+
+module.exports = { sequelize, PlayerUCP };
